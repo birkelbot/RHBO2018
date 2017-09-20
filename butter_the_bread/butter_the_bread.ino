c3fc2b4e0d36710bf7b29a4330af9ad3fd6bbe70
lucasrmal <lucasrmal@localhost>
2017-09-19 22:57:03 -0400

    Add motor control and start of servo

--- butter_the_bread/butter_the_bread.ino
+++ butter_the_bread/butter_the_bread.ino
@@ -2,14 +2,33 @@
 #include "MeOrion.h"
 #include <SoftwareSerial.h>
 
-#define NUM_INSTRUCTIONS 1
+#define NUM_INSTRUCTIONS 3
 
 #define LED_BUILTIN 13
 
+// Data
 MeBluetooth bluetooth(PORT_3);
 
-void setup() {
+// Motors
+MeDCMotor left_motor(M1);
+MeDCMotor right_motor(M2);
+
+// Servo
+MePort servo_port(PORT_1);
+Servo arm_servo;
+
+void setuarm_servop() {
+  // Setup.
   bluetooth.begin(115200);    //The factory default baud rate is 115200
+  arm_servo.attach(servo_port.pin1());
+
+  // Give a few blinks to show that the code is up and running.
+  for (int i = 0; i < 3; ++i) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+  }
 }
 
 void loop() {
@@ -24,11 +43,19 @@ void loop() {
   }
 
   // Read the instructions.
-  int led_state = bluetooth.read();
+  int left_motor_speed = bluetooth.read();
+  int right_motor_speed = bluetooth.read();
+  int servo = bluetooth.read();
 
-  if (led_state) {
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-  }
+  if (!checkMotorRange(left_motor_speed) || !checkMotorRange(right_motor_speed)) {
+    return;
+  } 
+  
+  left_motor.run(left_motor_speed);
+  right_motor.run(right_motor_speed);
 }
+
+bool checkMotorRange(int motor_speed) {
+  return motor_speed >= -255 && motor_speed <= 255;
+}
+
