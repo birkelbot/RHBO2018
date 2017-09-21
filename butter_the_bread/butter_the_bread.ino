07939fce560ce5fd3a34b26f6435fae7573ba872
Lucas Rioux-Maldague <lucasrmal@google.com>
2017-09-21 09:08:49 -0400

    Used `Serial` instead of `bluetooth`.

--- butter_the_bread/butter_the_bread.ino
+++ butter_the_bread/butter_the_bread.ino
@@ -1,13 +1,13 @@
 
-#include "MeOrion.h"
+#include <Arduino.h>
+#include <Wire.h>
 #include <SoftwareSerial.h>
+
+#include <MeMCore.h>
 
 #define NUM_INSTRUCTIONS 3
 
 #define LED_BUILTIN 13
-
-// Data
-MeBluetooth bluetooth(PORT_3);
 
 // Motors
 MeDCMotor left_motor(M1);
@@ -19,7 +19,7 @@ Servo arm_servo;
 
 void setup() {
   // Setup.
-  bluetooth.begin(115200);    //The factory default baud rate is 115200
+  Serial.begin(115200);    //The factory default baud rate is 115200
   arm_servo.attach(servo_port.pin1());
 
   // Give a few blinks to show that the code is up and running.
@@ -32,20 +32,20 @@ void setup() {
 }
 
 void loop() {
-  if (bluetooth.available() < NUM_INSTRUCTIONS + 1) {
+  if (Serial.available() < NUM_INSTRUCTIONS + 1) {
     return;
   }
   
-  int start = bluetooth.read();
+  int start = Serial.read();
   // Look for the start byte (255, or 0xFF)
   if (start != 255) {
     return;
   }
 
   // Read the instructions.
-  int left_motor_speed = bluetooth.read();
-  int right_motor_speed = bluetooth.read();
-  int servo = bluetooth.read();
+  int left_motor_speed = Serial.read();
+  int right_motor_speed = Serial.read();
+  int servo = Serial.read();
 
   if (!checkMotorRange(left_motor_speed) || !checkMotorRange(right_motor_speed)) {
     return;
