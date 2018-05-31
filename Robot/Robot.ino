cdc954643a900ccc0a31b571a176d342b4dfedb6
Matt Birkel <mbirkel91@gmail.com>
2018-05-30 23:03:05 -0400

    Updated robot code to use claw and bed, and buzz when backing

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -3,27 +3,39 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 #include <MeMCore.h>
+#include <MeOrion.h>
 
-#define NUM_INSTRUCTIONS 3
+#define NUM_INSTRUCTIONS 5
 #define MAX_MS_NO_COMMS 250
 
 #define LED_BUILTIN 13
 #define ARM_UP_POS 180
+#define CLAW_OPEN_POS 180
+#define BED_DOWN_POS 180
 
 // Motors
 MeDCMotor left_motor(M1);
 MeDCMotor right_motor(M2);
 
 // Servo
-MePort servo_port(PORT_1);
+MePort servo_port_arm(PORT_1);
+MePort servo_port_claw(PORT_1);
+MePort servo_port_bed(PORT_1);
 Servo arm_servo;
+Servo claw_servo;
+Servo bed_servo;
 
 void setup() {
   // Setup.
   Serial.begin(115200);    // The factory default baud rate is 115200
-  arm_servo.attach(servo_port.pin1());
+  arm_servo.attach(servo_port_arm.pin1());
   arm_servo.write(ARM_UP_POS);
 
+  claw_servo.attach(servo_port_claw.pin1());
+  claw_servo.write(CLAW_OPEN_POS);
+
+  bed_servo.attach(servo_port_bed.pin1());
+  bed_servo.write(BED_DOWN_POS);
   // Give a few blinks to show that the code is up and running.
   blink(3);
 }
@@ -55,10 +67,14 @@ void tryReadComms() {
   int left_motor_speed = Serial.read();
   int right_motor_speed = Serial.read();
   int arm_pos = Serial.read();
+  int claw_pos = Serial.read();
+  int bed_pos = Serial.read();
 
   setMotorSpeed(left_motor_speed, &left_motor);
   setMotorSpeed(right_motor_speed, &right_motor);
   arm_servo.write(arm_pos);
+  claw_servo.write(claw_pos);
+  bed_servo.write(bed_pos);
 }
 
 
@@ -68,6 +84,8 @@ void checkComms() {
     left_motor.run(0);
     right_motor.run(0);
     arm_servo.write(ARM_UP_POS);
+    claw_servo.write(CLAW_OPEN_POS);
+    bed_servo.write(BED_DOWN_POS);
     // Indicate that we have lost comms by turning off the on-board LED
     digitalWrite(LED_BUILTIN, LOW);
     Serial.println("No comms");
@@ -80,6 +98,12 @@ bool setMotorSpeed(int motor_speed, MeDCMotor* motor) {
     motor_speed *= 2;
     motor_speed -= 254;
     motor->run(motor_speed);
+
+    if (motor_speed < 0)
+      buzzer_on();
+    } else {
+      buzzer_off();
+    }
   }
 }
 
