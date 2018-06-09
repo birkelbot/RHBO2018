9ca833f1f1974bd1b88eb7fcb3fdb3d4489eec1e
Matt Birkel <mbirkel91@gmail.com>
2018-06-09 18:18:13 -0400

    Stripped  code to only include drive train motors

--- Be Fast/DriverStation/DriverStation.py
+++ Be Fast/DriverStation/DriverStation.py
@@ -15,12 +15,6 @@ joystickNum = 0
 
 joystickYDrive = 1
 joystickRDrive = 3
-joystickArm = 4
-joystickClaw = 5
-joystickAnalogTriggerArm = 2
-joystickAnalogTriggerClaw = 5
-joystickBed = 0
-joystickHorn = 1
 
 deadband = 0.10 # Deadband for the analog joystick.
 
@@ -39,9 +33,6 @@ def main():
 
     # Local variables
     prevdriveMtrCmds = {'left':0, 'right':0}
-    prevArmCmd = 0
-    prevClawCmd = 0
-    prevBedCmd = 0
     prevTimeSent = 0
     done = False
 
@@ -57,56 +48,13 @@ def main():
             # Get the drive motor commands for Arcade Drive
             driveMtrCmds = arcadeDrive(yRaw, rRaw)
 
-            # Get the arm motor command
-            armCmd = 0
-            clawCmd = 0
-            bedCmd = 0
-            
-            # Get arm, claw and bed commands
-            armUpBtn = joysticks[joystickNum].get_button(joystickArm)
-            clawCloseBtn = joysticks[joystickNum].get_button(joystickClaw)
-            analogTriggerRawArm = joysticks[joystickNum].get_axis(joystickAnalogTriggerArm)
-            analogTriggerRawClaw = joysticks[joystickNum].get_axis(joystickAnalogTriggerClaw)
-            bedDumpBtn = joysticks[joystickNum].get_button(joystickBed)
-            hornBtn = joysticks[joystickNum].get_button(joystickHorn)
-
-            # Arm
-            if (analogTriggerRawArm + 1 >= deadband):
-                armCmd = 1 # armMed
-            elif (armUpBtn):
-                armCmd = 2 # armHigh
-            else:
-                armCmd = 0 #armLow
-
-            # Claw
-            if (analogTriggerRawClaw + 1 >= deadband):
-                clawCmd = 1 # clawMed
-            elif (clawCloseBtn):
-                clawCmd = 0 # clawClosed
-            else:
-                clawCmd = 2 # clawOpen
-
-            # Bed
-            if (bedDumpBtn):
-                bedCmd = 1 # bedDump
-            else:
-                bedCmd = 0 # bedDown
-
-            # Assemble the bitwise command
-            bitwiseCmd = armCmd | clawCmd << 2 | bedCmd << 4 | hornBtn << 5
-
-            # Only send if the commands changed or if 200ms have elapsed
-            # if (prevdriveMtrCmds['left'] != driveMtrCmds['left'] or
-            #     prevdriveMtrCmds['right'] != driveMtrCmds['right'] or
-            #     prevArmCmd != armCmd or prevClawCmd != clawCmd or prevBedCmd != bedCmd or
-            #     time.time()*1000 > prevTimeSent + 200):
+            # Send every 200ms
             if (time.time()*1000 > prevTimeSent + 200):
 
-                print("Sending... L: ", driveMtrCmds['left'], ", R: ", driveMtrCmds['right'], ", A: ", armCmd, ", C: ", clawCmd, ", B: ", bedCmd)
+                print("Sending... L: ", driveMtrCmds['left'], ", R: ", driveMtrCmds['right'])
                 ser.write(chr(255))  # Start byte
                 ser.write(chr(driveMtrCmds['left']))
                 ser.write(chr(254-driveMtrCmds['right']))
-                ser.write(chr(bitwiseCmd))
 
                 prevdriveMtrCmds = driveMtrCmds
                 prevArmCmd = armCmd
@@ -209,59 +157,6 @@ def arcadeDrive(yIn, rIn):
     rightMtrCmdFinal = int(rightdriveMtrCmdScaled + zeroCommand)
 
     return {'left':leftMtrCmdFinal, 'right':rightMtrCmdFinal}
-
-
-############################################################
-## @brief  Function to compute the arm drive command
-## @param  aIn - raw joystick input from -1.0 to 1.0
-## @return the arm command
-############################################################
-def armDrive(aIn):
-
-    # Set output command range constants
-    zeroCommand = int(127)  # the default value that corresponds to no motor power
-    cmdRange = int(127)     # the maximum amount (+/-) that the command can vary from the zero command
-    maxCommand = cmdRange
-    minCommand = -cmdRange
-
-    # Set constants for the exponential function
-    endExpConst = 1.44 # don't change this unless you've really looked over the math
-
-    expConst = 1.5  # exponential growth coefficient of the Y-axis translation -- should be between 1.0-4.0
-    endpoint = 127  # maximum/minumum (+/-) for the Y-axis translation
-
-    # Set a base command (within the command range above) to overcome gearbox resistance at low drive speeds
-    baseCmd = int(5)
-
-    # Save the negative-ness, which will be re-applied after the exponential function is applied
-    if aIn < 0:
-        neg = -1
-    else:
-        neg = 1
-
-    # Apply a deadband
-    if abs(aIn) < deadband:
-        aIn = 0
-    
-    # Compute the motor command using the exponential function (zero-based)
-    aCmd = int(neg*(math.pow(math.e, math.pow(math.fabs(aIn), expConst)/endExpConst)-1)*endpoint) # zero-based
-
-    # Add an offset for the minimum command to overcome the gearboxes
-    if aCmd > 0:
-        aCmd = aCmd + baseCmd
-    elif aCmd < 0:
-        aCmd = aCmd - baseCmd
-
-    # If the command is greater than the maximum or less than the minimum, scale it back
-    if aCmd > maxCommand:
-        aCmd = maxCommand
-    elif aCmd < minCommand:
-        aCmd = minCommand
-
-    # Shift the command to be based on the zeroCommand (above)
-    aCmd = aCmd + zeroCommand
-
-    return aCmd
 
 
 ############################################################
